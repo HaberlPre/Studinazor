19abf49f04cb76d5521b15691e66c8fd078a2338
Juliane <juliane.gastell@gmail.com>
2017-09-05 13:35:30 +0200

    ToDoListe 1

---
+++ app/src/main/java/com/gastell_gehr_haberl/studinazor/ToDoItem.java
@@ -0,0 +1,46 @@
+package com.gastell_gehr_haberl.studinazor;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+
+/**
+ * Created by Juliane on 05.09.2017.
+ */
+
+public class ToDoItem implements Comparable<ToDoItem> {
+
+    private String name;
+    private GregorianCalendar date;
+
+    public ToDoItem(String name, int day, int month, int year) {
+        this.name = name;
+        date = new GregorianCalendar(year, month, day);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFormattedDate() {
+        DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT,
+                Locale.GERMANY);
+        return df.format(date.getTime());
+    }
+
+    public Date getDueDate() {
+        return date.getTime();
+    }
+
+    @Override
+    public int compareTo(ToDoItem newItem) {
+        return getDueDate().compareTo(newItem.getDueDate());
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getName() + ", Date: " + getFormattedDate();
+    }
+}
+
---
+++ app/src/main/java/com/gastell_gehr_haberl/studinazor/ToDoListeAdapter.java
@@ -0,0 +1,52 @@
+package com.gastell_gehr_haberl.studinazor;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Juliane on 05.09.2017.
+ */
+
+public class ToDoListeAdapter extends ArrayAdapter<ToDoItem> {
+
+    private ArrayList<ToDoItem> todoList;
+    private Context context;
+    private TextView item;
+    private TextView date;
+
+    public ToDoListeAdapter(Context context, ArrayList<ToDoItem> listItems) {
+        super(context, R.layout.todolist_itemlist, listItems);
+        this.context = context;
+        this.todoList = listItems;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = convertView;
+
+        if (v == null) {
+            LayoutInflater inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(R.layout.todolist_itemlist, null);
+
+        }
+        ToDoItem task = todoList.get(position);
+
+        if (task != null) {
+            item = (TextView) v.findViewById(R.id.text_task);
+            date = (TextView) v.findViewById(R.id.text_date);
+
+            item.setText(task.getName());
+            date.setText(task.getFormattedDate());
+        }
+
+        return v;
+    }
+}
